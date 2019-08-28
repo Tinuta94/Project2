95ce4ed30b8df4e04a5b7857c7ad33d79165a5b6
Tinuta94 <cristina.crestenciuc@gmail.com>
2019-08-28 10:01:33 -0400

    revert changes

--- controllers/products.js
+++ controllers/products.js
@@ -32,21 +32,21 @@ productRouter.get('/:productId/edit', function (req, res) {
 productRouter.post('/', function (req, res) {
   productsApi.addProduct(req.body)
     .then((addProduct) => {
-      res.redirect(`/homeScreen/${login._id}`)
+      res.redirect('/homeScreen/${login._id}')
     })
 })
 
 productRouter.put('/:productId', function (req, res) {
   productsApi.updateProduct(req.params.productId, req.body)
     .then(() => {
-      res.redirect(`/homeScreen/${login._id}`)
+      res.redirect('/homeScreen/${login._id}')
     })
 })
 
 productRouter.delete('/:productId', function (req, res) {
   productsApi.deleteProduct(req.params.productId)
     .then((deletedProduct) => {
-      res.redirect(`/homeScreen/${login._id}`)
+      res.redirect('/homeScreen/${login._id}')
     })
 })
 
